09b37d97bd2c9ab39714e673ed85c09b1467e0fe
Randwow <makeev.n.00@mail.ru>
2019-05-04 11:21:17 +0300

    two-dimensional array

--- src/arrayExample/array.java
+++ src/arrayExample/array.java
@@ -5,15 +5,43 @@ public class array {
 //			for (int i = 0; i < array.length; i++ ) {
 //				System.out.println(array[i]); 
 //		}
-		int i = 10;
-		int j = 10;
-		int[][] array = new int[i][j];
-		for (i = 0; i < array.length; i++) {
-			for (j = 0; j < array.length; j++) {
-				array[i][j] = ( i > j ) ? i : j;
-				System.out.print( " " + array[i][j]);
-			} 
-			System.out.print("\n");
+		int size = 10;
+		int size1 = 10;
+		int sum=0;
+		int max = 0;
+		int[][] array = new int[size][size1];
+		for(int i = 0; i < array.length; i++) { // Заполнение массива
+			for(int j = 0; j < array.length; j++ ) {
+			array[i][j] = (int) Math.round(Math.random()*100); //Заполнение массива рандомными числами  
+			}
 		}
+		int min = array[0][0];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+//				array[i][j] = ( i > j ) ? i : j;
+				sum += array[i][j];
+				System.out.print( "  " + array[i][j]);
+				if(array[i][j] > max ) {
+					max = array[i][j];		
+				}
+				if(array[i][j] < min) {
+					min = array[i][j];
+				}
+			} 		
+			System.out.print(" Сумма каждого ряда = " + sum +" ");
+			System.out.print("\n");
+		} 
+		System.out.println("\n" + "    Общая сумма = " + sum);
+		System.out.println("    Наибольшее число:= " + max);
+		System.out.println("    Наименьшее число:= " + min);		
+		System.out.println("     Главная диагональ");
+		for(int i = 0; i < array.length; i++) { 
+		System.out.print(" " + array[i][i] + " "); //Вывод на экран главную диагональ 
+			}
+		System.out.print("\n");
+		System.out.println("     Побочная диагональ");
+		for(int i = 0; i < array.length; i++) { 
+			System.out.print(" " + array[i][size - 1 - i ] + " "); //Вывод на экран побочную диагональ 
+		}	
 	}
 }
